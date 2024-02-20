d9e5cb1670244cd941b7c0c501452a819d49caeb
Devesh9446 <Srivastavadevesh43@gmail.com>
2024-02-21 04:52:03 +0530

    finished

--- Client/src/featureSection/EmblaCarousel.jsx
+++ Client/src/featureSection/EmblaCarousel.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
-// import FeatureCard from "./FeatureCard";
+import styled from 'styled-components';
+
+const jsonFile = [
+  {
+    index: 1,
+    image: "../../assets/embla_corousel/delivery_person.svg",
+    heading: "Doorstep Delivery",
+    content:
+      "A Platform to doorstep Deliver the School amenities such as Booksets , Uniforms, Stationaries ,etc.",
+  },
+  {
+    index: 2,
+    image: "../../assets/embla_corousel/phone_showcase.svg",
+    heading: "Optimize Admissions",
+    content:
+      "Online application, fee, and document management. Clear information, minimal paperwork.",
+  },
+  {
+    index: 3,
+    image: "../../assets/embla_corousel/parents.svg",
+    heading: "Parent-School Communication Bridge",
+    content:
+      "Instant notifications, event news, and online discussions. Keeps parents involved and students backed.",
+  },
+  {
+    index: 4,
+    image: "../../assets/embla_corousel/school_promotion.svg",
+    heading: "School Promotion",
+    content:
+      "Consolidate ad campaigns in one spot. Attractive banners, expanded reach, boost enrollments.",
+  },
+  {
+    index: 5,
+    image: "../../assets/embla_corousel/admin.svg",
+    heading: "Automate Admin Tasks",
+    content:
+      "Ditch the paperwork. Automate routine tasks, free up time for what matters most: your students.",
+  },
+];
 
 const EmblaCarousel = (props) => {
-  const { slides, options, arr, onIndexChange } = props;
+  const { options, onIndexChange } = props;
   const [emblaRef, emblaApi] = useEmblaCarousel(options, [
     Autoplay({ delay: 3000 }),
   ]);
-  const [currentIndex, setCurrentIndex] = React.useState(0);
+
   React.useEffect(() => {
     if (emblaApi) {
       const onSelect = () => {
@@ -22,92 +60,54 @@ const EmblaCarousel = (props) => {
       return () => emblaApi.off("select", onSelect);
     }
   }, [emblaApi, onIndexChange]);
-  const isMobile = window.innerWidth <= 768;
+
   return (
-    <div className="embla w-full">
-      <div className="embla__viewport " ref={emblaRef}>
+    <CarouselContainer className="embla w-full">
+      <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container ">
-          {slides.map((index) => (
-            <div className="embla__slide " key={index}>
-              <img
-                className={"embla__slide__img" + isMobile ? "" : ""}
-                src={arr(index)}
-                alt="Your alt text"
-              />
-              {/* <FeatureCard
-                imageSrc={arr(index)}
-                heading="Heading"
-                text="Hello"
-              /> */}
+          {jsonFile.map((data) => (
+            <div className="embla__slide flex justify-center" key={data.index}>
+              <Container>
+                <div className=" w-full h-auto bg-white relative rounded-3xl shadow flex flex-wrap items-center justify-center">
+                  <StyledImage
+                    src={data.image}
+                    className="w-1/2 md:w-auto"
+                  />
+                  {/* <div className="w-80 h-80 md:w-0 md:h-0 bg-gradient-to-b from-sky-400 to-sky-600 rounded-e-full" /> */}
+                  <div className="flex flex-col md:w-auto md:ml-4">
+                    <h1 className="text-black font-headline text-2xl pb-3">{data.heading}</h1>
+                    <img src="../../assets/embla_corousel/line.svg" className="pb-3" />
+                    <h1>{data.content}</h1>
+                  </div>
+                </div>
+              </Container>
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </CarouselContainer>
   );
 };
 
+const CarouselContainer = styled.div`
+  .embla__slide {
+    width: 100%;
+  }
+`;
+
+const Container = styled.div`
+  @media (max-width: 768px) {
+    .embla__slide {
+      width: 100%;
+    }
+  }
+`;
+
+const StyledImage = styled.img`
+  @media (max-width: 770px) {
+    width: 100px; 
+    height:auto;
+  }
+`;
+
 export default EmblaCarousel;
-
-// import React from 'react';
-// import useEmblaCarousel from 'embla-carousel-react';
-// import Autoplay from 'embla-carousel-autoplay';
-// import imageByIndex from './imageByIndex';
-// import imagebyMobile from './imagebyMobile';
-
-// const EmblaCarousel = (props) => {
-// 	const { slides, options } = props;
-// 	const [emblaRef] = useEmblaCarousel(options, [Autoplay({ delay: 3000 })]);
-
-// 	return (
-// 			// <div className="embla  ">
-// 			// 	<div
-// 			// 		className="embla__viewport "
-// 			// 		ref={emblaRef}>
-// 			// 		<div className="embla__container ">
-// 			// 			{slides.map((index) => (
-// 			// 				<div
-// 			// 					className="embla__slide "
-// 			// 					key={index}>
-// 			// 					<img
-// 			// 						className="embla__slide__img  h-[100%]"
-// 			// 						src={imageByIndex(index)}
-// 			// 						alt="Your alt text"
-// 			// 					/>
-// 			// 				</div>
-// 			// 			))}
-// 			// 		</div>
-// 			// 	</div>
-// 			// </div>
-// 			<div className="embla w-full">
-// 				<div
-// 					className="embla__viewport "
-// 					ref={emblaRef}>
-// 					<div className="embla__container ">
-// 						{slides.map((index) => (
-// 							<div
-// 								className="embla__slide "
-// 								key={index}>
-// 								<img
-// 									className="embla__slide__img  "
-// 									src={imagebyMobile(index)}
-// 									alt="Your alt text"
-// 								/>
-// 							</div>
-// 						))}
-// 					</div>
-// 				</div>
-// 			</div>
-
-// 			{/* <div className="flex justify-center items-center md:mr-12 md-[100%] lg:w-[40%]">
-// 				<section className="sandbox__carousel">
-// 					<EmblaClientCarousel
-// 						slides={SLIDES}
-// 						options={OPTIONS}
-// 					/>
-// 				</section>
-// 			</div> */}
-// 	);
-// };
-
-// export default EmblaCarousel;
